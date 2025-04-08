1983b6ee80f6d263b1ef4aa1836c1a4f91255316
Theiremi <remibonnard@laposte.net>
2025-04-08 16:26:59 +0200

    Some begign cleaning

--- libsddc/libsddc.h
+++ libsddc/libsddc.h
@@ -68,101 +68,83 @@ enum LEDColors {
 
 /* basic functions */
 int sddc_get_device_count();
-
 int sddc_get_device_info(struct sddc_device_info **sddc_device_infos);
-
 int sddc_free_device_info(struct sddc_device_info *sddc_device_infos);
 
-sddc_t *sddc_open(int index);
+sddc_t*	sddc_open(int index);
+void	sddc_close(sddc_t *t);
 
-void sddc_close(sddc_t *t);
-
-enum SDDCStatus sddc_get_status(sddc_t *t);
-
-enum SDDCHWModel sddc_get_hw_model(sddc_t *t);
-
-const char *sddc_get_hw_model_name(sddc_t *t);
+enum SDDCStatus		sddc_get_status(sddc_t *t);
+enum SDDCHWModel	sddc_get_hw_model(sddc_t *t);
+const char*			sddc_get_hw_model_name(sddc_t *t);
 
 uint16_t sddc_get_firmware(sddc_t *t);
 
 const double *sddc_get_frequency_range(sddc_t *t);
 
-enum RFMode sddc_get_rf_mode(sddc_t *t);
-
-int sddc_set_rf_mode(sddc_t *t, enum RFMode rf_mode);
+enum RFMode	sddc_get_rf_mode(sddc_t *t);
+int			sddc_set_rf_mode(sddc_t *t, enum RFMode rf_mode);
 
 
 /* LED functions */
-int sddc_led_on(sddc_t *t, uint8_t led_pattern);
-
-int sddc_led_off(sddc_t *t, uint8_t led_pattern);
-
-int sddc_led_toggle(sddc_t *t, uint8_t led_pattern);
+int sddc_led_on		(sddc_t *t, uint8_t led_pattern);
+int sddc_led_off	(sddc_t *t, uint8_t led_pattern);
+int sddc_led_toggle	(sddc_t *t, uint8_t led_pattern);
 
 
 /* ADC functions */
 int sddc_get_adc_dither(sddc_t *t);
-
 int sddc_set_adc_dither(sddc_t *t, int dither);
 
 int sddc_get_adc_random(sddc_t *t);
-
 int sddc_set_adc_random(sddc_t *t, int random);
+
+int sddc_get_adc_sample
 
 
 /* HF block functions */
-double sddc_get_hf_attenuation(sddc_t *t);
+double	sddc_get_hf_attenuation(sddc_t *t);
+int		sddc_set_hf_attenuation(sddc_t *t, double attenuation);
 
-int sddc_set_hf_attenuation(sddc_t *t, double attenuation);
-
-int sddc_get_hf_bias(sddc_t *t);
-
-int sddc_set_hf_bias(sddc_t *t, int bias);
+int		sddc_get_hf_bias(sddc_t *t);
+int		sddc_set_hf_bias(sddc_t *t, int bias);
 
 
 /* VHF block and VHF/UHF tuner functions */
-double sddc_get_tuner_frequency(sddc_t *t);
+double	sddc_get_tuner_frequency(sddc_t *t);
+int		sddc_set_tuner_frequency(sddc_t *t, double frequency);
 
-int sddc_set_tuner_frequency(sddc_t *t, double frequency);
+int		sddc_get_tuner_rf_attenuations(sddc_t *t, const double *attenuations[]);
+double	sddc_get_tuner_rf_attenuation(sddc_t *t);
+int		sddc_set_tuner_rf_attenuation(sddc_t *t, double attenuation);
 
-int sddc_get_tuner_rf_attenuations(sddc_t *t, const double *attenuations[]);
+int		sddc_get_tuner_if_attenuations(sddc_t *t, const double *attenuations[]);
+double	sddc_get_tuner_if_attenuation(sddc_t *t);
+int		sddc_set_tuner_if_attenuation(sddc_t *t, double attenuation);
 
-double sddc_get_tuner_rf_attenuation(sddc_t *t);
-
-int sddc_set_tuner_rf_attenuation(sddc_t *t, double attenuation);
-
-int sddc_get_tuner_if_attenuations(sddc_t *t, const double *attenuations[]);
-
-double sddc_get_tuner_if_attenuation(sddc_t *t);
-
-int sddc_set_tuner_if_attenuation(sddc_t *t, double attenuation);
-
-int sddc_get_vhf_bias(sddc_t *t);
-
-int sddc_set_vhf_bias(sddc_t *t, int bias);
+int		sddc_get_vhf_bias(sddc_t *t);
+int		sddc_set_vhf_bias(sddc_t *t, int bias);
 
 
 /* streaming functions */
 typedef void (*sddc_read_async_cb_t)(uint32_t data_size, uint8_t *data,
 										void *context);
 
-double sddc_get_sample_rate(sddc_t *t);
+double	sddc_get_sample_rate(sddc_t *t);
+int		sddc_set_sample_rate(sddc_t *t, double sample_rate);
 
-int sddc_set_sample_rate(sddc_t *t, double sample_rate);
-
-int sddc_set_async_params(sddc_t *t, uint32_t frame_size, 
+int		sddc_set_async_params(sddc_t *t, uint32_t frame_size, 
 							uint32_t num_frames, sddc_read_async_cb_t callback,
 							void *callback_context);
 
-int sddc_start_streaming(sddc_t *t);
+int		sddc_start_streaming(sddc_t *t);
+int		sddc_stop_streaming(sddc_t *t);
 
-int sddc_handle_events(sddc_t *t);
+int		sddc_handle_events(sddc_t *t);
 
-int sddc_stop_streaming(sddc_t *t);
+int		sddc_reset_status(sddc_t *t);
 
-int sddc_reset_status(sddc_t *t);
-
-int sddc_read_sync(sddc_t *t, uint8_t *data, int length, int *transferred);
+int		sddc_read_sync(sddc_t *t, uint8_t *data, int length, int *transferred);
 
 #ifdef __cplusplus
 }
