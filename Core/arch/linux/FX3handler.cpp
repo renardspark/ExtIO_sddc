8042f575aa2af0d747f93a02416dd280f2e60f07
Theiremi <remibonnard@laposte.net>
2025-06-03 21:50:38 +0200

    Improving logging in fx3handler

--- Core/arch/linux/FX3handler.cpp
+++ Core/arch/linux/FX3handler.cpp
@@ -14,17 +14,23 @@
 
 fx3class *CreateUsbHandler()
 {
+    TracePrintln(TAG, "");
+
     return new fx3handler();
 }
 
 fx3handler::fx3handler()
 {
+    TracePrintln(TAG, "");
+
     usb_device_infos = nullptr;
     dev = nullptr;
 }
 
 fx3handler::~fx3handler()
 {
+    TracePrintln(TAG, "");
+
     Close();
 }
 
@@ -32,7 +38,7 @@ bool fx3handler::Open(uint8_t dev_index)
 {
     TracePrintln(TAG, "%d", dev_index);
     dev = usb_device_open(dev_index, firmware_data, firmware_size);
-    DebugPrintf(TAG, "Open device with dev_index=%d, dev=%p\n", dev_index, dev);
+    DebugPrintln(TAG, "Open device with dev_index=%d, dev=%p", dev_index, dev);
 
     usleep(5000);
     Control(STOPFX3, (uint8_t)0);
@@ -54,27 +60,36 @@ bool fx3handler::Close(void)
 
 bool fx3handler::Control(FX3Command command, uint8_t data)
 {
+    TracePrintln(TAG, "%d, %d", command, data);
+
     return usb_device_control(this->dev, command, 0, 0, (uint8_t *)&data, sizeof(data), 0) == 0;
 }
 
 bool fx3handler::Control(FX3Command command, uint32_t data)
 {
+    TracePrintln(TAG, "%d, %d", command, data);
+
     return usb_device_control(this->dev, command, 0, 0, (uint8_t *)&data, sizeof(data), 0) == 0;
 }
 
 bool fx3handler::Control(FX3Command command, uint64_t data)
 {
+    TracePrintln(TAG, "%d, %ld", command, data);
+
     return usb_device_control(this->dev, command, 0, 0, (uint8_t *)&data, sizeof(data), 0) == 0;
 }
 
 bool fx3handler::SetArgument(uint16_t index, uint16_t value)
 {
+    TracePrintln(TAG, "%d, %d", index, value);
+
     uint8_t data = 0;
     return usb_device_control(this->dev, SETARGFX3, value, index, (uint8_t *)&data, sizeof(data), 0) == 0;
 }
 
 bool fx3handler::GetHardwareInfo(uint32_t *data)
 {
+    TracePrintln(TAG, "%p", data);
 #ifdef _DEBUG
     uint8_t enable_debug = 1;
 #else
@@ -92,7 +107,7 @@ void fx3handler::StartStream(ringbuffer<int16_t> &samples_buf)
     stream = streaming_open_async(this->dev, inputbuffer->getBlockSize() * sizeof(int16_t), concurrentTransfers, PacketRead, this);
     //samples_buf.setBlockSize(streaming_framesize(stream) / sizeof(int16_t));
 
-    DebugPrintf(TAG, "Samples buffer blocksize: %d\n", samples_buf.getBlockSize());
+    DebugPrintln(TAG, "Samples buffer blocksize: %d", samples_buf.getBlockSize());
 
     // Start background thread to poll the events
     run = true;
@@ -114,6 +129,8 @@ void fx3handler::StartStream(ringbuffer<int16_t> &samples_buf)
 
 void fx3handler::StopStream()
 {
+    TracePrintln(TAG, "");
+
     run = false;
     poll_thread.join();
 
@@ -134,6 +151,8 @@ void fx3handler::PacketRead(uint32_t data_size, uint8_t *data, void *context)
 
 bool fx3handler::ReadDebugTrace(uint8_t *pdata, uint8_t len)
 {
+    TracePrintln(TAG, "%p, %d", pdata, len);
+
     //TODO : This doesn't work
     return true;
     return usb_device_control(this->dev, READINFODEBUG, pdata[0], 0, (uint8_t *)pdata, len, 1) == 0;
@@ -141,6 +160,8 @@ bool fx3handler::ReadDebugTrace(uint8_t *pdata, uint8_t len)
 
 bool fx3handler::Enumerate(unsigned char &idx, char *lbuf)
 {
+    TracePrintln(TAG, "%d, %s", idx, lbuf);
+
     if (idx >= usb_device_count_devices()) return false;
 
     if (usb_device_infos == nullptr) {
@@ -161,6 +182,8 @@ bool fx3handler::Enumerate(unsigned char &idx, char *lbuf)
 
 sddc_err_t fx3handler::SearchDevices()
 {
+    TracePrintln(TAG, "");
+
     if (usb_device_infos != nullptr) {
         usb_device_free_device_list(usb_device_infos);
     }
@@ -172,6 +195,8 @@ sddc_err_t fx3handler::SearchDevices()
 
 size_t fx3handler::GetDeviceListLength()
 {
+    TracePrintln(TAG, "");
+
     return usb_device_count_devices();
 }
 
@@ -182,6 +207,8 @@ bool fx3handler::GetDevice(
     char *serial,
     size_t serial_len)
 {
+    TracePrintln(TAG, "%d, %p, %ld, %p, %ld", idx, name, name_len, serial, serial_len);
+
     if (idx >= usb_device_count_devices()) return false;
 
     if (usb_device_infos == nullptr) {
